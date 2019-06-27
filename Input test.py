1348ddfff41055f3856b17a7695c49dae666b87b
Josh Huang <johuang10@gmail.com>
2019-06-27 14:49:16 -0700

    added more testing files of me trying python

---
+++ Input test.py
@@ -0,0 +1,5 @@
+while True:
+	n = input("Positive Number: ")
+	if n > 0:
+		print(n)
+		break
---
+++ switching variables.py
@@ -0,0 +1,6 @@
+x = 1
+y = 2
+
+print("x is {x}, and y is {y}")
+x, y = y, x
+print(f"but now x is {x}, and y {y}")
