cd97a484c85177b48a6b8172e051cdbf49b62c3a
Josh Huang <johuang10@gmail.com>
2019-07-11 11:17:43 -0700

    more trash files from testing

--- Perspective_Testing/Python_Test.py
+++ Perspective_Testing/Python_Test.py
@@ -1,29 +1,22 @@
-#!flask/bin/python
-
-import sys
-
-from flask import Flask, render_template, request, redirect, Response
-import random, json
-
+# app.py
+from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def output():
-	# serve index template
-	return render_template('index.html', name='Joe')
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
 
-@app.route('/receiver', methods = ['POST'])
-def worker():
-	# read json + reply
-	data = request.get_json()
-	result = ''
+    # POST request
+    if request.method == 'POST':
+        print('Incoming..')
+        print(request.get_json())  # parse as JSON
+        return 'OK', 200
 
-	for item in data:
-		# loop over every row
-		result += str(item['make']) + '\n'
+    # GET request
+    else:
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
 
-	return result
-
-if __name__ == '__main__':
-	# run!
-	app.run()
+@app.route('/test')
+def test_page():
+    # look inside `templates` and serve `index.html`
+    return render_template('index.html')
