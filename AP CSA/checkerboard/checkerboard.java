d79b78906cc138aeb089304f0bb0e8ac06ba8dd4
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2020-02-27 09:14:28 -0800

    added one space for spacing

--- AP CSA/checkerboard/checkerboard.java
+++ AP CSA/checkerboard/checkerboard.java
@@ -38,7 +38,7 @@ public class checkerboard {
     }
 
     private static void printBoard() {
-        System.out.println("    0     1     2     3     4     5     6     7   ");
+        System.out.println("     0     1     2     3     4     5     6     7   ");
         System.out.println("  _________________________________________________");
         int rowNum = 0;
         for (String[] row : board) {
